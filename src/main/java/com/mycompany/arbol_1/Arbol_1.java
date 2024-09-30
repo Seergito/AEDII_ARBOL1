f7d86e023ee84f9cd7eb336fcdd91614a05fb507
Seergito912 <seergitox90@gmail.com>
2024-09-30 10:46:30 +0200

    F1

---
+++ src/main/java/com/mycompany/arbol_1/Arbol_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arbol_1;
+
+/**
+ *
+ * @author estudante
+ */
+public class Arbol_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
